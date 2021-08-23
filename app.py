3add6e5a8532425aa5b61d424b47819f8ac8db81
Rashmi <rashmi.manandhar7@gmail.com>
2021-08-23 09:46:02 -0400

    feat: add CRUD operation for actors

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 from models import *
 
+ROWS_PER_PAGE = 10
+
 
 def create_app(test_config=None):
     setup_db(app)
@@ -14,6 +16,104 @@ def create_app(test_config=None):
 
     CORS(app)
 
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers',
+                             'Content-Type,Authorization')
+        response.headers.add('Access-Control-Allow-Methods',
+                             'GET,PATCH,POST,DELETE,OPTIONS')
+        return response
+
+    def paginate_results(request, selection):
+        page = request.args.get('page', 1, type=int)
+
+        start = (page - 1) * ROWS_PER_PAGE
+        end = start + ROWS_PER_PAGE
+
+        objects = [object.format for object in selection]
+        return objects[start:end]
+
+    @app.route('/actors')
+    def get_actors():
+        actors = Actor.query.all()
+        actors_paginated = paginate_results(request, actors)
+
+        if len(actors_paginated) == 0:
+            abort(404)
+
+        return jsonify({
+            'success': True,
+            'actors': actors_paginated
+        })
+
+    @app.route('/actors', methods=['POST'])
+    def insert_actors():
+        body = request.get_json()
+
+        if not body:
+            abort(400)
+        name = body.get('name', None)
+        age = body.get('age', None)
+
+        gender = body.get('gender', 'Other')
+
+        if not name or not age:
+            abort(422)
+
+        new_actor = (Actor(name=name, age=age, gender=gender))
+        new_actor.insert()
+
+        return jsonify({
+            'success': True,
+            'created': new_actor.id
+        })
+
+    @app.route('/actors/<actor_id>', methods=['PATCH'])
+    def edit_actors(actor_id):
+        body = request.get_json()
+
+        if not actor_id or not body:
+            abort(400)
+
+        actor_to_update = Actor.query.filter(
+            Actor.id == actor_id).one_or_none()
+
+        if not actor_to_update:
+            abort(404)
+
+        name = body.get('name', actor_to_update.name)
+        age = body.get('age', actor_to_update.age)
+        gender = body.get('gender', actor_to_update.gender)
+
+        actor_to_update.name = name
+        actor_to_update.age = age
+        actor_to_update.gender = gender
+
+        actor_to_update.update()
+
+        return jsonify({
+            'success': True,
+            'updated': actor_to_update.id,
+            'actor': [actor_to_update.format]
+        })
+
+    @app.route('/actors/<actor_id>', methods=['DELETE'])
+    def delete_actors(actor_id):
+        if not actor_id:
+            abort(400)
+
+        actor_to_delete = Actor.query.filter(
+            Actor.id == actor_id).one_or_none()
+
+        if not actor_to_delete:
+            abort(404)
+
+        actor_to_delete.delete()
+
+        return jsonify({
+            'success': True,
+            'deleted': actor_id
+        })
     return app
 
 
